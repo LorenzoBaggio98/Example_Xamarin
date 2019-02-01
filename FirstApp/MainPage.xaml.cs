e2583d6db11ff2eb36003e5fcbce17958a243135
Lorenzo <lrz.baggio@outlook.it>
2019-02-01 14:53:40 +0100

    First commit

---
+++ FirstApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        //APP CENTER
+        void OnStart()
+        {
+            AppCenter.Start("android=b5ac94ac-9688-44e9-a1b1-7f32368d1247;",
+                  typeof(Analytics), typeof(Crashes));
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
